d88bf7be871e7b37f2e0d426275c972f1ec651fc
AlexCombat <combat0705@yandex.ru>
2020-02-15 23:43:24 +0300

    Added HTTP service querying and gRPC health check for the server

--- calculator_client.py
+++ calculator_client.py
@@ -6,16 +6,57 @@ import grpc
 import example_pb2
 import example_pb2_grpc
 
-port = 50051
+from argparse import ArgumentParser
+import requests
+
+consul_port = None
+resolver = None
+
+
+# The default (most popular) way of name resolving in grpc is through DNS protocol.
+# This function is a dirty hack meant to workaround the issue with grpc being unable to resolve
+# DNS queries with authorities (couldn't find any meaningful info in the web).
+def resolve_service_address(service_name):
+    # Query string parameter 'passing' filters the query to look for only healthy instances
+    response = requests.get('http://127.0.0.1:{}/v1/catalog/service/{}?passing'.format(consul_port, service_name))
+    if response.status_code is not 200:
+        raise requests.HTTPError('Failed to find service: {}'.format(response.text))
+
+    service_info = response.json()[0]
+    return '{}:{}'.format(service_info['Address'], service_info['ServicePort'])
+
+
+def get_service_channel(service_name):
+    if resolver == 'dns':
+        # To retrieve service port from DNS query we need to request SRV record
+        # Link below leads to gRPC proposal that makes use of DNS SRV records
+        # https://github.com/grpc/proposal/blob/master/A5-grpclb-in-dns.md
+        # gRPC doc article about name resolution
+        # https://github.com/grpc/grpc/blob/master/doc/naming.md
+        service_uri = 'dns:///127.0.0.1:{port}/_{service_name}.service.consul'.format(port=consul_port,
+                                                                                      service_name=service_name)
+    elif resolver == 'http':
+        service_uri = resolve_service_address(service_name)
+
+    return grpc.insecure_channel(service_uri)
+
 
 if __name__ == '__main__':
     logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+
+    parser = ArgumentParser()
+    parser.add_argument('--resolver', type=str,
+                        help='Method of resolving service names from consul: DNS (default) or HTTP (workaround)',
+                        choices=['dns', 'http'], default='dns')
+    args = parser.parse_args()
+    resolver = args.resolver
+    # 8600 - Consul DNS interface port
+    # 8500 - Consul HTTP API port
+    consul_port = 8600 if resolver == 'dns' else 8500
+
     logging.info('Starting calculator client configuration...')
 
-    # channel = grpc.insecure_channel('localhost:{}'.format(port))
-    channel = grpc.insecure_channel('dns:///127.0.0.1:8600/calculator.service.consul.:50001')
-    # channel = grpc.insecure_channel('http://localhost:8500/v1/catalog/service/calculator')
-    stub = example_pb2_grpc.CalculatorStub(channel)
+    calculator_stub = example_pb2_grpc.CalculatorStub(get_service_channel('calculator'))
 
     while True:
         # logging.info('Enter the number and the power')
@@ -26,7 +67,7 @@ if __name__ == '__main__':
 
         logging.info('Requesting {}^{} calculation'.format(value, power))
 
-        result = stub.CalculatePower(
+        result = calculator_stub.CalculatePower(
             example_pb2.Operands(num1=example_pb2.Number(value=value), num2=example_pb2.Number(value=power)))
 
         logging.info('Result: {}'.format(result.value))
--- calculator_server.py
+++ calculator_server.py
@@ -8,7 +8,12 @@ import grpc
 import example_pb2
 import example_pb2_grpc
 
+from grpc_health.v1 import health
+from grpc_health.v1 import health_pb2
+from grpc_health.v1 import health_pb2_grpc
+
 port = 50051
+health_check_servicer = None
 
 class CalculatorServices(example_pb2_grpc.CalculatorServicer):
     def __init__(self):
@@ -21,18 +26,37 @@ class CalculatorServices(example_pb2_grpc.CalculatorServicer):
         logging.info('Calculate {}^{}: {}'.format(value, power, result))
         return example_pb2.Number(value=result)
 
+HealthCheckStatus = health_pb2.HealthCheckResponse
+
+def set_health_check_status(status):
+    health_check_servicer.set('', status)
+
 
 def serve():
     logging.info('Starting configuration...')
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     example_pb2_grpc.add_CalculatorServicer_to_server(
         CalculatorServices(), server)
+
+    set_health_check_status(HealthCheckStatus.UNKNOWN)
+    health_pb2_grpc.add_HealthServicer_to_server(health_check_servicer, server)
+
     server.add_insecure_port('[::]:{}'.format(port))
     server.start()
     logging.info('Started listening on port {}'.format(port))
+    set_health_check_status(HealthCheckStatus.SERVING)
+
     server.wait_for_termination()
+    set_health_check_status(HealthCheckStatus.NOT_SERVING)
 
 
 if __name__ == '__main__':
     logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+
+    # Implementation of gRPC health checking
+    # https://github.com/grpc/grpc/blob/master/doc/health-checking.md
+    # Consul can be configured to execute this check to track the service's health status
+    # https://www.consul.io/docs/agent/checks.html
+    health_check_servicer = health.HealthServicer()
+
     serve()
